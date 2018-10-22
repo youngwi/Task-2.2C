31b70daabd9998e340c39968d5e919c50ca06ec6
youngwi <42164100+youngwi@users.noreply.github.com>
2018-10-23 02:15:10 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._2C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string string1 = "Conflict";
+           string string2 = "Conflict";
+
+            Console.WriteLine(string1 + string2);
+            Console.ReadLine();
+        }
+    }
+}
